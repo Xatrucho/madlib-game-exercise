8b0f69a6fe8d4e4f5d0e80ccbea91e64969bc6c9
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2021-12-08 14:31:44 -0600

    first commit

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("")
+verb2 = input("")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer programming is the {adj}! It makes me excited all the time because \
+I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
